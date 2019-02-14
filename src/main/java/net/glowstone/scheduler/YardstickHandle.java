98c394127229d38d6dcf1f37081853603356ee64
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-02-14 09:55:37 +0100

    added stupid brackets

--- src/main/java/net/glowstone/scheduler/YardstickHandle.java
+++ src/main/java/net/glowstone/scheduler/YardstickHandle.java
@@ -4,12 +4,12 @@ public static class YardstickHandle {
 	private static final boolean enabled = false;
 
 	public static void start(String first, String second) {
-		if enabled {
+		if (enabled) {
 			com.atlarge.yscollector.YSCollector.start(first, second); // YSCollector
 		} 
 	}
 	public static void stop(String str) {
-		if enabled {
+		if (enabled) {
             com.atlarge.yscollector.YSCollector.stop(str);
 		}
 	}
