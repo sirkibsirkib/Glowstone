d86a87b8b81d7955a2f7bfaf5e57a3f92ff603af
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-02-15 16:23:07 +0100

    more

--- src/main/java/net/glowstone/entity/GlowPlayer.java
+++ src/main/java/net/glowstone/entity/GlowPlayer.java
@@ -1049,10 +1049,16 @@ public class GlowPlayer extends GlowHumanEntity implements Player {
             worldLock.writeLock().unlock();
         }
 
-        YSCollector.pushSummaryValue("yes_sync", "Number of outward messages that triggered sync", (double) yes_sync);
-        YSCollector.pushSummaryValue("no_sync", "Number of outward messages that DIDNT trigger sync", (double) no_sync);
-        server.logger.info(yes_sync + " YES syncs");
-        server.logger.info(no_sync + " NO syncs");
+        if (yes_sync > 0) {
+            YSCollector.pushSummaryValue("yes_sync", "Number of outward messages that triggered sync", (double) yes_sync);
+            server.logger.info(yes_sync + " YES syncs");
+        }
+
+        if (no_sync > 0) {
+            YSCollector.pushSummaryValue("no_sync", "Number of outward messages that DIDNT trigger sync", (double) no_sync);
+            server.logger.info(no_sync + " NO syncs");   
+        }
+
 
         if (passengerChanged) {
             session.send(new SetPassengerMessage(SELF_ID, getPassengers().stream()
